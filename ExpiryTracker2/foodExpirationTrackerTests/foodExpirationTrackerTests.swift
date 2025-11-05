d263ccb3abc71330f63215d51535955b6fb40b5a
Heet191205 <hshah2@uab.edu>
2025-11-05 10:59:26 -0600

    Add files via upload

---
+++ ExpiryTracker2/foodExpirationTrackerTests/foodExpirationTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  foodExpirationTrackerTests.swift
+//  foodExpirationTrackerTests
+//
+//  Created by Mahir Patel on 7/30/25.
+//
+
+import Testing
+@testable import foodExpirationTracker
+
+struct foodExpirationTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
